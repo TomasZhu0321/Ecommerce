ad50c29890716e896dd9991e1d7e5e2648d6438b
ThomasZhu <tomaszhu0321@gmail.com>
2022-08-31 21:34:41 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+
+import {
+  HomePage,
+  AboutPage,
+  ProductsPage,
+  SingleProductPage,
+  CheckoutPage,
+} from "./pages";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route path="/about" exact>
+          <AboutPage />
+        </Route>
+        <Route path="/products" exact>
+          <ProductsPage />
+        </Route>
+        <Route path="/products/:productId" children={<SingleProductPage/>} exact>
+        </Route>
+        <Route path="/checkout" exact>
+          <CheckoutPage />
+        </Route>
+        <Route path="*" >
+          <HomePage />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import HomePage from "./HomePage";
+import AboutPage from "./AboutPage";
+import ProductsPage from "./ProductsPage";
+import CheckoutPage from "./CheckoutPage";
+import SingleProductPage from "./SingleProductPage";
+
+export {
+    HomePage,
+    AboutPage,
+    ProductsPage,
+    CheckoutPage,
+    SingleProductPage
+
+}
