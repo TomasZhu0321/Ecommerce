f9b93067c96525eac3af536ab5f553348c658477
ThomasZhu <tomaszhu0321@gmail.com>
2022-09-02 20:56:43 -0400

    fetch data complete

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -1,3 +1,4 @@
+import { act } from "react-dom/test-utils";
 import {
   SIDEBAR_OPEN,
   SIDEBAR_CLOSE,
@@ -7,17 +8,34 @@ import {
   GET_SINGLE_PRODUCT_BEGIN,
   GET_SINGLE_PRODUCT_SUCCESS,
   GET_SINGLE_PRODUCT_ERROR,
-} from '../actions'
+} from "../actions";
 
 const products_reducer = (state, action) => {
-    if (action.type === SIDEBAR_OPEN){
-      return {...state,isSidebarOpen:true}
-    }
-    if (action.type === SIDEBAR_CLOSE){
-      return {...state,isSidebarOpen:false}
-    }
+  if (action.type === SIDEBAR_OPEN) {
+    return { ...state, isSidebarOpen: true };
+  }
+  if (action.type === SIDEBAR_CLOSE) {
+    return { ...state, isSidebarOpen: false };
+  }
+  if (action.type === GET_PRODUCTS_BEGIN) {
+    return { ...state, products_loading: true };
+  }
+  if (action.type === GET_PRODUCTS_SUCCESS) {
+    const featured_products = action.payload.filter((product) => 
+      product.featured === true
+    );
+    return {
+      ...state,
+      products_loading: false,
+      featured_products: featured_products,
+      products:action.payload
+    };
+  }
+  if (action.type === GET_PRODUCTS_ERROR) {
+    return{...state,products_loading: false,products_error:true}
+  }
 
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+  throw new Error(`No Matching "${action.type}" - action type`);
+};
 
-export default products_reducer
+export default products_reducer;
