f25760216babb30eb63b0a7a0ccc90438714238d
ThomasZhu <tomaszhu0321@gmail.com>
2022-09-02 21:33:47 -0400

    format price

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,6 +1,10 @@
 import React from 'react'
 const Error = () => {
-  return <h4>error element</h4>
+  return (
+    <div className="section section-center">
+      <h2>Error...</h2>
+    </div>
+  )
 }
 
 export default Error
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,9 @@
-export const formatPrice = () => {}
+export const formatPrice = (number) => {
+    const NewNumber = Intl.NumberFormat('en-US',{
+        style:'currency',
+        currency:'CAD',
+    }).format(number/100)
+    return NewNumber
+}
 
 export const getUniqueValues = () => {}
