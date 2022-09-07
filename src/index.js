cc0ea1ca83895de351c1608429f2262a08901c4a
ThomasZhu <tomaszhu0321@gmail.com>
2022-09-06 23:00:53 -0400

    checkout

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 //qeDF3ifHcdyzyZOjoQ92CtBhfXQLNnHi
 ReactDOM.render(
   <Auth0Provider
-    domain="dev-efsy4f79.us.auth0.com"
-    clientId="qeDF3ifHcdyzyZOjoQ92CtBhfXQLNnHi"
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
     redirectUri={window.location.origin}
     cacheLocation="localstorage"
   >
